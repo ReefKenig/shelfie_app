7736e24af024a0a60534dfc754701a6c4977e58f
Reef Kenig <rkenig@aol.com>
2025-08-23 21:38:58 +0300

    Basic web pages

---
+++ app/index.jsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+
+import Logo from "../assets/img/Lavenza.png";
+
+import ThemedView from "../components/ThemedView";
+
+const Home = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+      <Link href={"/about"} style={styles.link}>
+        About us
+      </Link>
+
+      <Link href={"/contact"} style={styles.link}>
+        Contact us
+      </Link>
+    </ThemedView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  img: {
+    height: 200,
+    width: 250,
+    marginVertical: 20,
+  },
+  link: { marginVertical: 10, borderBottomWidth: 1 },
+});
